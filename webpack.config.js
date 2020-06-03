b4e75bef573b5f84a67e2688c588bbc273a714e0
Brett Barber <brettab3@hotmail.com>
2020-06-03 17:15:53 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isDev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDev ? 'development' : 'production',
+    entry: {
+        app: './src/app.jsx'
+    },
+    devtool: isDev ? 'eval-source-map' : 'none',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Alertify',
+            template: './src/template.html',
+            minify: true
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.s[ac]ss/i,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
